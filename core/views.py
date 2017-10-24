8055570ee4fe8778e2f1211c793034cbd67299e2
Felipe <felipepereira937@gmail.com>
2017-10-23 22:06:02 -0200

    conexao com banco

--- core/views.py
+++ core/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
+from core.models import Curso
 
 def index(requisicao):
     contexto = {
-        "cursos":[
-            {"nome":"Desenvolvimento de Sistemas", "tipo":"Graduação"},
-            {"nome":"Banco de Dados", "tipo":"Graduação"},
-            {"nome":"Internet Das Coisas", "tipo":"Graduação"},
-            {"nome":"TecWeb", "tipo":"Graduação"}
-        ],
+        "cursos": Curso.objects.all(),
         "faculdade": "Faculdade Impacta",
         "Pagina":"Homepage",
         "usuario": "Felipe",
@@ -18,43 +14,6 @@ def index(requisicao):
 
 def cursos(request):
     context = {
-        "cursos": [
-            {
-                "nome": "Graduação em Sistemas de Informação",
-                 "alt": "Imagem do Curso de SI",
-                 "path": "detalheCurso.html",
-                 "img":"img/box_curso_si.jpg"},
-
-            {
-                "nome": "Graduação em Administração",
-                "alt": "Imagem do Curso de ADM",
-                "path": "detalheCurso.html",
-                "img":"img/box_curso_adm.jpeg"
-            },
-            {
-                "nome": "Graduação em Banco de Dados",
-                "alt": "Imagem do Curso de BD",
-                "path": "detalheCurso.html",
-                "img":"img/box_curso_bd.jpeg"
-            },
-            {
-                "nome": "Graduação em Análise e Desenvolvimento de Sistemas",
-                "alt": "Imagem do Curso de ADS",
-                "path": "detalheCurso.html",
-                "img":"img/box_curso_ads.jpg"
-            },
-            {
-                "nome": "Graduação em Jogos Digitais",
-                "alt": "Imagem do Curso de Jogos",
-                "path": "detalheCurso.html",
-                "img":"img/box_curso_jogos.jpeg"
-            },
-            {
-                "nome": "Graduação em Gestão da Tecnologia da Informação",
-                "alt": "Imagem do Curso de GTI",
-                "path": "detalheCurso.html",
-                "img":"img/box_curso_gti.jpeg"
-            }
-        ]
+        "cursos": Curso.objects.all()
     }
     return render(request, 'cursos.html', context)
